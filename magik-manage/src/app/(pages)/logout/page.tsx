77858cdba7f0df133e2987f4bb4714168fd5d549
codergb <betweent@126.com>
2024-06-29 15:56:22 +0800

    登出

--- magik-manage/src/app/(pages)/logout/page.tsx
+++ magik-manage/src/app/(pages)/logout/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 import { request } from '@/app/utils/request';
+import { message } from 'antd';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function Logout(): React.ReactNode {
 	const router = useRouter();
+	message.destroy();
+	message.warning('登录已失效');
 	request({
 		url: '/api/logout',
 	}).then((res) => {
